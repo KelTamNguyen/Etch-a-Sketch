abad71cefedbcd2686e3e108fb89846f2e424f15
Kelvin Nguyen <keltamnguyen@yahoo.com>
2022-01-29 18:35:48 -0600

    add grid

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 console.log('The script is running');
+
+const DEFAULT_SIZE = 16;
+var grid = document.querySelector('#grid');
+
+function setUpGrid(size) {
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`; // why does this not work?
+
+    for (let i = size; i < size * size; i++) {
+        const square = document.createElement('div');
+        const randomR = Math.floor(Math.random() * 256);
+        const randomG = Math.floor(Math.random() * 256);
+        const randomB = Math.floor(Math.random() * 256);
+        square.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+        square.addEventListener('mouseover', (event) => {
+            event.target.style.backgroundColor = '#333333';
+        })
+        grid.appendChild(square);
+    }
+}
+
+window.onload = () => {
+    setUpGrid(DEFAULT_SIZE);
+}
