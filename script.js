3606e8c932ce922b023f74172c7f984d81504f9f
KelTamNguyen <keltamnguyen@yahoo.com>
2024-12-16 21:30:05 -0600

    Refactor Javascript code and implement extra credit (darkening)
    The "var" keyword has been refactored out of script.js entirely. DOM
    variables are now consts and internal variables are let/const.
    
    Squares now darken gradually by 10% when moused over as specified in the extra credit section.

--- script.js
+++ script.js
@@ -1,42 +1,24 @@
 console.log('The script is running');
 
-const DEFAULT_COLOR = '#333333';
+// Variables
+const DEFAULT_COLOR = 'rgb(51,51,51)';
 const DEFAULT_SIZE = 16;
 const DEFAULT_MODE = 'color';
 const DEFAULT_CANVAS_MODE = 'light';
-var currentColor = DEFAULT_COLOR;
-var currentSize = DEFAULT_SIZE;
-var currentColoringMode = DEFAULT_MODE;
+let currentColor = DEFAULT_COLOR;
+let currentSize = DEFAULT_SIZE;
+let currentColoringMode = DEFAULT_MODE;
 
-var grid = document.querySelector('#grid');
-var colorInput = document.querySelector('#color-input');
-var resetButton = document.getElementById('reset');
-var rainbowButton = document.getElementById('rainbow');
-var colorButton = document.getElementById('color');
-var modalBg = document.querySelector('.modal-bg');
-var modalClose = document.querySelector('.modal-close');
-var sizeInput = document.querySelector('#size');
-var applyBtn = document.querySelector('#apply');
-
-colorInput.addEventListener('input', (e) => {
-    currentColor = e.target.value;
-});
-applyBtn.addEventListener('click', () => {
-    if (sizeInput.value !== null) {
-        if (sizeInput.value <= 100) {
-            setNewSize(sizeInput.value);
-        } 
-        else if (sizeInput.value > 100) {
-            setNewSize(100);
-        }
-        setUpGrid(sizeInput.value);
-        closeModal();
-    }
-});
-modalClose.addEventListener('click', closeModal);
-resetButton.addEventListener('click', clearGrid);
-rainbowButton.addEventListener('click', () => setColoringMode('rainbow'));
-colorButton.addEventListener('click', () => setColoringMode('color'))
+// DOM References
+const grid = document.querySelector('#grid');
+const colorInput = document.querySelector('#color-input');
+const resetButton = document.getElementById('reset');
+const rainbowButton = document.getElementById('rainbow');
+const colorButton = document.getElementById('color');
+const modalBg = document.querySelector('.modal-bg');
+const modalClose = document.querySelector('.modal-close');
+const sizeInput = document.querySelector('#size');
+const applyBtn = document.querySelector('#apply');
 
 function setNewSize(size) {
     currentSize = size;
@@ -60,7 +42,7 @@ function setColoringMode(coloringMode) {
 
 function clearGrid() {
     grid.innerHTML = '';
-    setUpGrid(currentSize);
+    initializeGrid(currentSize);
     console.log(currentSize);
     openModal();
 }
@@ -74,14 +56,17 @@ function closeModal() {
     sizeInput.value= "";
 }
 
-function setUpGrid(size) {
+function initializeGrid(size) {
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
     for (let i = 0; i < size * size; i++) {
         const square = document.createElement('div');
         square.style.backgroundColor = 'rgb(216, 216, 216)';
-        square.addEventListener('mouseover', colorSquare)
+        square.style.opacity = "0";
+        square.dataset.isColored = false;
+        square.dataset.color = 'rgb(216, 216, 216)';
+        square.addEventListener('mouseover', colorSquare);
         grid.appendChild(square);
     }
 }
@@ -89,15 +74,49 @@ function setUpGrid(size) {
 function colorSquare(event) {
     if (currentColoringMode === 'color') {
         event.target.style.backgroundColor = currentColor;
+        event.target.setAttribute('data-color', 'rgb(51, 51, 51)');
     }
     else if (currentColoringMode === 'rainbow') {
-        const randomR = Math.floor(Math.random() * 256);
-        const randomG = Math.floor(Math.random() * 256);
-        const randomB = Math.floor(Math.random() * 256);
+        let randomR = Math.floor(Math.random() * 256);
+        let randomG = Math.floor(Math.random() * 256);
+        let randomB = Math.floor(Math.random() * 256);
         event.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+        event.target.setAttribute('data-color',  `rgb(${randomR}, ${randomG}, ${randomB})`);
     }
+
+    if (event.target.dataset.isColored === 'true') {
+        let currentOpacity = Number(getComputedStyle(event.target).getPropertyValue('opacity'));
+        event.target.style.opacity = (currentOpacity + 0.1).toString();
+    }
+    
+    event.target.setAttribute('data-is-colored', 'true');
 }
 
+function setCurrentColor(color) {
+    currentColor = color;
+}
+
+colorInput.addEventListener('input', (e) => {
+    setCurrentColor(e.target.value);
+});
+applyBtn.addEventListener('click', () => {
+    if (sizeInput.value !== null) {
+        if (sizeInput.value <= 100) {
+            setNewSize(sizeInput.value);
+        } 
+        else if (sizeInput.value > 100) {
+            setNewSize(100);
+        }
+        initializeGrid(sizeInput.value);
+        closeModal();
+    }
+});
+modalClose.addEventListener('click', closeModal);
+resetButton.addEventListener('click', clearGrid);
+rainbowButton.addEventListener('click', () => setColoringMode('rainbow'));
+colorButton.addEventListener('click', () => setColoringMode('color'));
+
 window.onload = () => {
-    setUpGrid(DEFAULT_SIZE);
+    initializeGrid(DEFAULT_SIZE);
+    colorButton.classList.add('active-button');
 }
